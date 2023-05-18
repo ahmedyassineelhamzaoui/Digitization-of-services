97273828c17d9ac36dbb5643ecbc86d43ba7b2ae
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-05-18 01:45:07 +0100

    modify the action buttons in users file

--- resources/views/layouts/dashboard/user-dash.blade.php
+++ resources/views/layouts/dashboard/user-dash.blade.php
@@ -22,12 +22,14 @@
                 <td>{{$item->roles[0]->name}}</td>
                 <th>
                     <div class="d-flex align-items-center">
-                        <button class="btn">
-                            <i class="fa-regular fa-trash-can text-danger"></i>
-                        </button>
-                        <button class="btn">
-                            <i class="fa-solid fa-pen-to-square text-success"></i>
-                        </button>
+                        <div class="d-flex align-items-center">
+                            <button class="btn btn-danger  me-1">
+                                <i class="fa-regular fa-trash-can "></i>
+                            </button>
+                            <button class="btn btn-warning ">
+                                <i class="fa-solid fa-pen-to-square "></i>
+                            </button>
+                        </div>
                     </div>
                 </th>
               </tr>
