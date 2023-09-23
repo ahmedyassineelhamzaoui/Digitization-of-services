66c81022a12d957c4d79c174eb60a82733e23763
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-09-23 14:18:43 +0100

    update the footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="copyright">
-        <p>Copyright © 2023, SOGEPIE Developed by
+        <p>Copyright © 2023, SONAPIE Developed by
             <span class="text-danger">&hearts; </span
                 >MTS GROUP AFRICA
             <span class="text-danger"> &hearts; </span></p>
